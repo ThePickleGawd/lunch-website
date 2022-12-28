f395bc45b6b5bece0d5d7ac7eb0d8315d2e3379d
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2022-12-27 22:47:12 -0800

    fix build bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import { Session } from "next-auth";
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<{
+  session: Session;
+}>) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
