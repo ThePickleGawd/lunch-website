2c364fb817e0c7656a703fc823251bae6ed10be2
Dylan Lu <42813970+ThePickleGawd@users.noreply.github.com>
2023-02-21 22:38:50 -0800

    init dev branch

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          extralight: "#C1C8E4",
+          light: "#84CEEB",
+          DEFAULT: "#5AB9EA",
+          dark: "#5680E9",
+          extradark: "#8860D0",
+        },
+      },
+    },
   },
   plugins: [],
 };
